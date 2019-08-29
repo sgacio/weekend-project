10ca57893d4436b67f30327e79f1b81ce83e4f77
Stephen Gacio <sgacio@mail.usf.edu>
2019-08-29 15:45:07 -0400

    base setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HomePage from './components/HomePage'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <main>
+        <nav>
+          <header>This is Albert's TV Show APP</header>
+        </nav>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage}></Route>
+          </Switch>
+        </Router>
+      </main>
+    )
   }
 }
 
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const HomePage = () => {
+  const [movie, setMovie] = useState([])
+  // const [name, setName] = useState('')
+
+  const grabData = async () => {
+    const resp = await axios.get(
+      'https://api.themoviedb.org/3/tv/top_rated?api_key=%3C%3C2d3a8fbb2336ce29522e69bb7f40bffa%3E%3E&language=en-US&page=1'
+    )
+    console.log(resp.data.results)
+    setMovie(resp.data.results)
+  }
+
+  useEffect(() => {
+    grabData()
+  }, [])
+
+  return (
+    <>
+      <ul>
+        {movie.map(result => {
+          return <li>{result.original_name}</li>
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default HomePage
